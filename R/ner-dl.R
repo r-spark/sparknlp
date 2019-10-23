814688407c110839ba3ee0507f75bdec0f9ad411
Dave Kincaid <kincaid.dave@gmail.com>
2019-10-23 17:34:55 -0500

    NER DL model

---
+++ R/ner-dl.R
@@ -0,0 +1,166 @@
+#' Spark NLP NerDLApproach Named Entity Recognition Deep Learning annotator
+#'
+#' This Named Entity recognition annotator allows to train generic NER model based on Neural Networks. 
+#' Its train data (train_ner) is either a labeled or an external CoNLL 2003 IOB based spark dataset with Annotations 
+#' columns. Also the user has to provide word embeddings annotation column.
+#' 
+#' Neural Network architecture is Char CNNs - BiLSTM - CRF that achieves state-of-the-art in most datasets.
+#' See \url{https://nlp.johnsnowlabs.com/docs/en/annotators#ner-dl}
+#' 
+#' @template roxlate-nlp-algo
+#' @template roxlate-inputs-output-params
+#' @param label_col If DatasetPath is not provided, this Seq[Annotation] type of column should have labeled data per token
+#' @param max_epochs Maximum number of epochs to train
+#' @param lr Initial learning rate
+#' @param po Learning rate decay coefficient. Real Learning Rate: lr / (1 + po * epoch)
+#' @param batch_size Batch size for training
+#' @param dropout Dropout coefficient
+#' @param verbose Verbosity level
+#' @param include_confidence whether to include confidence values
+#' @param random_seed Random seed
+#' 
+#' @return When \code{x} is a \code{spark_connection} the function returns a NerDLApproach estimator.
+#' When \code{x} is a \code{ml_pipeline} the pipeline with the NerDLApproach added. When \code{x}
+#' is a \code{tbl_spark} a transformed \code{tbl_spark}  (note that the Dataframe passed in must have the input_cols specified).
+#' 
+#' @export
+nlp_ner_dl <- function(x, input_cols, output_col,
+                 label_col = NULL, max_epochs = NULL, lr = NULL, po = NULL, batch_size = NULL, dropout = NULL, 
+                 verbose = NULL, include_confidence = NULL, random_seed = NULL, uid = random_string("ner_dl_")) {
+  UseMethod("nlp_ner_dl")
+}
+
+#' @export
+nlp_ner_dl.spark_connection <- function(x, input_cols, output_col,
+                 label_col = NULL, max_epochs = NULL, lr = NULL, po = NULL, batch_size = NULL, dropout = NULL, 
+                 verbose = NULL, include_confidence = NULL, random_seed = NULL, uid = random_string("ner_dl_")) {
+  args <- list(
+    input_cols = input_cols,
+    output_col = output_col,
+    label_col = label_col,
+    max_epochs = max_epochs,
+    lr = lr,
+    po = po,
+    batch_size = batch_size,
+    dropout = dropout,
+    verbose = verbose,
+    include_confidence = include_confidence,
+    random_seed = random_seed,
+    uid = uid
+  ) %>%
+  validator_nlp_ner_dl()
+
+  # TODO: there is a bug in sparklyr that hinders passing arguments to methods that require Float types.
+  # (https://github.com/rstudio/sparklyr/issues/2151)
+  jobj <- sparklyr::spark_pipeline_stage(
+    x, "com.johnsnowlabs.nlp.annotators.ner.dl.NerDLApproach",
+    input_cols = args[["input_cols"]],
+    output_col = args[["output_col"]],
+    uid = args[["uid"]]
+  ) %>%
+    sparklyr::jobj_set_param("setLabelColumn", args[["label_col"]])  %>%
+    sparklyr::jobj_set_param("setMaxEpochs", args[["max_epochs"]])  %>%
+    #sparklyr::jobj_set_param("setLr", args[["lr"]])  %>%
+    #sparklyr::jobj_set_param("setPo", args[["po"]])  %>%
+    sparklyr::jobj_set_param("setBatchSize", args[["batch_size"]])  %>%
+    #sparklyr::jobj_set_param("setDropout", args[["dropout"]])  %>%
+    sparklyr::jobj_set_param("setVerbose", args[["verbose"]])  %>%
+    sparklyr::jobj_set_param("setIncludeConfidence", args[["include_confidence"]]) %>%
+    sparklyr::jobj_set_param("setRandomSeed", args[["random_seed"]]) 
+
+  new_nlp_ner_dl(jobj)
+}
+
+#' @export
+nlp_ner_dl.ml_pipeline <- function(x, input_cols, output_col,
+                 label_col = NULL, max_epochs = NULL, lr = NULL, po = NULL, batch_size = NULL, dropout = NULL, 
+                 verbose = NULL, include_confidence = NULL, random_seed = NULL, uid = random_string("ner_dl_")) {
+
+  stage <- nlp_ner_dl.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    label_col = label_col,
+    max_epochs = max_epochs,
+    lr = lr,
+    po = po,
+    batch_size = batch_size,
+    dropout = dropout,
+    verbose = verbose,
+    include_confidence = include_confidence,
+    random_seed = random_seed,
+    uid = uid
+  )
+
+  sparklyr::ml_add_stage(x, stage)
+}
+
+#' @export
+nlp_ner_dl.tbl_spark <- function(x, input_cols, output_col,
+                 label_col = NULL, max_epochs = NULL, lr = NULL, po = NULL, batch_size = NULL, dropout = NULL, 
+                 verbose = NULL, include_confidence = NULL, random_seed = NULL, uid = random_string("ner_dl_")) {
+  stage <- nlp_ner_dl.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    label_col = label_col,
+    max_epochs = max_epochs,
+    lr = lr,
+    po = po,
+    batch_size = batch_size,
+    dropout = dropout,
+    verbose = verbose,
+    include_confidence = include_confidence,
+    random_seed = random_seed,
+    uid = uid
+  )
+
+  stage %>% sparklyr::ml_fit(x)
+}
+#' @import forge
+validator_nlp_ner_dl <- function(args) {
+  args[["input_cols"]] <- cast_string_list(args[["input_cols"]])
+  args[["output_col"]] <- cast_string(args[["output_col"]])
+  args[["label_col"]] <- cast_nullable_string(args[["label_col"]])
+  args[["max_epochs"]] <- cast_nullable_integer(args[["max_epochs"]])
+  args[["lr"]] <- cast_nullable_double(args[["lr"]])
+  args[["po"]] <- cast_nullable_double(args[["po"]])
+  args[["batch_size"]] <- cast_nullable_integer(args[["batch_size"]])
+  args[["dropout"]] <- cast_nullable_double(args[["dropout"]])
+  args[["verbose"]] <- cast_nullable_integer(args[["verbose"]])
+  args[["include_confidence"]] <- cast_nullable_logical(args[["include_confidence"]])
+  args[["random_seed"]] <- cast_nullable_integer(args[["random_seed"]])
+  args
+}
+
+new_nlp_ner_dl <- function(jobj) {
+  sparklyr::new_ml_estimator(jobj, class = "nlp_ner_dl")
+}
+
+#' Load a pretrained Spark NLP NER DL model
+#' 
+#' Create a pretrained Spark NLP \code{NerDLModel} model
+#' 
+#' @template roxlate-pretrained-params
+#' @template roxlate-inputs-output-params
+#' @export
+nlp_ner_dl_pretrained <- function(sc, input_cols, output_col, include_confidence = NULL,
+                                   name = NULL, lang = NULL, remote_loc = NULL) {
+  args <- list(
+    input_cols = input_cols,
+    output_col = output_col
+  )
+  
+  args[["input_cols"]] <- forge::cast_string_list(args[["input_cols"]])
+  args[["output_col"]] <- forge::cast_string(args[["output_col"]])
+  args[["include_confidence"]] <- forge::cast_nullable_logical(args[["include_confidence"]])
+  
+  model_class <- "com.johnsnowlabs.nlp.annotators.ner.dl.NerDLModel"
+  model <- pretrained_model(sc, model_class, name, lang, remote_loc)
+  spark_jobj(model) %>%
+    sparklyr::jobj_set_param("setInputCols", args[["input_cols"]]) %>% 
+    sparklyr::jobj_set_param("setOutputCol", args[["output_col"]]) %>%
+    sparklyr::jobj_set_param("setIncludeConfidence", args[["include_confidence"]])
+  
+  new_ml_transformer(model)
+}
---
+++ tests/testthat/testthat-ner-dl.R
@@ -0,0 +1,73 @@
+setup({
+  sc <- testthat_spark_connection()
+  text_tbl <- testthat_tbl("test_text")
+  
+  train_data_file <- here::here("tests", "testthat", "data", "crf-eng.train.small")
+  conll_data <- nlp_conll_read_dataset(sc, train_data_file)
+  embeddings <- nlp_word_embeddings_pretrained(sc, output_col = "embeddings")
+  train_data <- ml_transform(embeddings, conll_data)
+
+  # These lines should set a pipeline that will ultimately create the columns needed for testing the annotator
+  assembler <- nlp_document_assembler(sc, input_col = "text", output_col = "document")
+  sentdetect <- nlp_sentence_detector(sc, input_cols = c("document"), output_col = "sentence")
+  tokenizer <- nlp_tokenizer(sc, input_cols = c("sentence"), output_col = "token")
+  word_embeddings <- nlp_word_embeddings_pretrained(sc, output_col = "embeddings")
+
+  pipeline <- ml_pipeline(assembler, sentdetect, tokenizer, word_embeddings)
+  test_data <- ml_fit_and_transform(pipeline, text_tbl)
+
+  assign("sc", sc, envir = parent.frame())
+  assign("pipeline", pipeline, envir = parent.frame())
+  assign("test_data", test_data, envir = parent.frame())
+  assign("train_data", train_data, envir = parent.frame())
+})
+
+teardown({
+  rm(sc, envir = .GlobalEnv)
+  rm(pipeline, envir = .GlobalEnv)
+  rm(test_data, envir = .GlobalEnv)
+  rm(train_data, envir = .GlobalEnv)
+})
+
+test_that("ner_dl param setting", {
+  test_args <- list(
+    input_cols = c("string1", "string2", "string3"),
+    output_col = "string1",
+    label_col = "string1",
+    max_epochs = 5,
+    #lr = 0.001,
+    #po = 0.001,
+    batch_size = 100,
+    #dropout = 0.6,
+    verbose = 0,
+    include_confidence = TRUE,
+    random_seed = 100
+  )
+
+  test_param_setting(sc, nlp_ner_dl, test_args)
+})
+
+test_that("nlp_ner_dl spark_connection", {
+  test_annotator <- nlp_ner_dl(sc, input_cols = c("sentence", "token", "embeddings"), output_col = "ner", label_col = "label")
+  fit_model <- ml_fit(test_annotator, train_data)
+  expect_equal(invoke(spark_jobj(fit_model), "getOutputCol"), "ner")
+})
+
+test_that("nlp_ner_dl ml_pipeline", {
+  test_annotator <- nlp_ner_dl(pipeline, input_cols = c("sentence", "token", "embeddings"), output_col = "ner", label_col = "label")
+  fit_pipeline <- ml_fit(test_annotator, train_data)
+  transformed_data <- ml_transform(fit_pipeline, test_data)
+  expect_true("ner" %in% colnames(transformed_data))
+})
+
+test_that("nlp_ner_dl tbl_spark", {
+  fit_model <- nlp_ner_dl(train_data, input_cols = c("sentence", "token", "embeddings"), output_col = "ner", label_col = "label")
+  expect_equal(invoke(spark_jobj(fit_model), "getOutputCol"), "ner")
+})
+
+test_that("nlp_ner_dl pretrained", {
+  model <- nlp_ner_dl_pretrained(sc, input_cols = c("sentence", "token", "embeddings"), output_col = "ner")
+  transformed_data <- ml_transform(model, test_data)
+  expect_true("ner" %in% colnames(transformed_data))
+})
+
