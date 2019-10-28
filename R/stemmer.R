3b14f5a5e5cb50fd3fe23357d0a8eb2e332dbd29
Dave Kincaid <kincaid.dave@gmail.com>
2019-10-28 16:36:06 -0500

    Stemmer implementation

---
+++ R/stemmer.R
@@ -0,0 +1,80 @@
+#' Spark NLP Stemmer
+#'
+#' Spark ML transformer that returns hard-stems out of words with the objective of retrieving the meaningful part of the word
+#' See \url{https://nlp.johnsnowlabs.com/docs/en/annotators#stemmer}
+#' 
+#' @template roxlate-nlp-algo
+#' @template roxlate-inputs-output-params
+#' @param language 
+#' 
+#' @export
+nlp_stemmer <- function(x, input_cols, output_col,
+                 language = NULL,
+                 uid = random_string("stemmer_")) {
+  UseMethod("nlp_stemmer")
+}
+
+#' @export
+nlp_stemmer.spark_connection <- function(x, input_cols, output_col,
+                 language = NULL,
+                 uid = random_string("stemmer_")) {
+  args <- list(
+    input_cols = input_cols,
+    output_col = output_col,
+    language = language,
+    uid = uid
+  ) %>%
+  validator_nlp_stemmer()
+
+  jobj <- sparklyr::spark_pipeline_stage(
+    x, "com.johnsnowlabs.nlp.annotators.Stemmer",
+    input_cols = args[["input_cols"]],
+    output_col = args[["output_col"]],
+    uid = args[["uid"]]
+  ) %>%
+    sparklyr::jobj_set_param("setLanguage", args[["language"]]) 
+
+  new_nlp_stemmer(jobj)
+}
+
+#' @export
+nlp_stemmer.ml_pipeline <- function(x, input_cols, output_col,
+                 language = NULL,
+                 uid = random_string("stemmer_")) {
+
+  stage <- nlp_stemmer.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    language = language,
+    uid = uid
+  )
+
+  sparklyr::ml_add_stage(x, stage)
+}
+
+#' @export
+nlp_stemmer.tbl_spark <- function(x, input_cols, output_col,
+                 language = NULL,
+                 uid = random_string("stemmer_")) {
+  stage <- nlp_stemmer.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    language = language,
+    uid = uid
+  )
+
+  stage %>% sparklyr::ml_transform(x)
+}
+#' @import forge
+validator_nlp_stemmer <- function(args) {
+  args[["input_cols"]] <- cast_string_list(args[["input_cols"]])
+  args[["output_col"]] <- cast_string(args[["output_col"]])
+  args[["language"]] <- cast_nullable_string(args[["language"]])
+  args
+}
+
+new_nlp_stemmer <- function(jobj) {
+  sparklyr::new_ml_transformer(jobj, class = "nlp_stemmer")
+}
---
+++ tests/testthat/testthat-stemmer.R
@@ -0,0 +1,50 @@
+setup({
+  sc <- testthat_spark_connection()
+  text_tbl <- testthat_tbl("test_text")
+
+  # These lines should set a pipeline that will ultimately create the columns needed for testing the annotator
+  assembler <- nlp_document_assembler(sc, input_col = "text", output_col = "document")
+  sentdetect <- nlp_sentence_detector(sc, input_cols = c("document"), output_col = "sentence")
+  tokenizer <- nlp_tokenizer(sc, input_cols = c("sentence"), output_col = "token")
+
+  pipeline <- ml_pipeline(assembler, sentdetect, tokenizer)
+  test_data <- ml_fit_and_transform(pipeline, text_tbl)
+
+  assign("sc", sc, envir = parent.frame())
+  assign("pipeline", pipeline, envir = parent.frame())
+  assign("test_data", test_data, envir = parent.frame())
+})
+
+teardown({
+  rm(sc, envir = .GlobalEnv)
+  rm(pipeline, envir = .GlobalEnv)
+  rm(test_data, envir = .GlobalEnv)
+})
+
+test_that("stemmer param setting", {
+  test_args <- list(
+    input_cols = c("string1"),
+    output_col = "string1",
+    language = "string1"
+  )
+
+  test_param_setting(sc, nlp_stemmer, test_args)
+})
+
+test_that("nlp_stemmer spark_connection", {
+  test_annotator <- nlp_stemmer(sc, input_cols = c("token"), output_col = "stem")
+  transformed_data <- ml_transform(test_annotator, test_data)
+  expect_true("stem" %in% colnames(transformed_data))
+})
+
+test_that("nlp_stemmer ml_pipeline", {
+  test_annotator <- nlp_stemmer(pipeline, input_cols = c("token"), output_col = "stem")
+  transformed_data <- ml_fit_and_transform(test_annotator, test_data)
+  expect_true("stem" %in% colnames(transformed_data))
+})
+
+test_that("nlp_stemmer tbl_spark", {
+  transformed_data <- nlp_stemmer(test_data, input_cols = c("token"), output_col = "stem")
+  expect_true("stem" %in% colnames(transformed_data))
+})
+
