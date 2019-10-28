19326cbafeeff1a3cc822b0549827ff51e424e58
Dave Kincaid <kincaid.dave@gmail.com>
2019-10-28 13:40:08 -0500

    Update to SparkNLP 2.3.0

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,9 @@
 # Get a pretrained model.
 # The model_class is the Scala class for the model.
 pretrained_model <- function(sc, model_class, name = NULL, lang = NULL, remote_loc = NULL) {
-  module <- invoke_static(sc, paste0(model_class, "$"), "MODULE$")
-  default_name <- invoke(module, "pretrained$default$1")
-  default_lang <- invoke(module, "pretrained$default$2")
-  default_remote_loc <- invoke(module, "pretrained$default$3")
+  default_name <- invoke_static(sc, model_class, "defaultModelName")
+  default_lang <- invoke_static(sc, model_class, "defaultLang")
+  default_remote_loc <- invoke_static(sc, model_class, "defaultLoc")
   
   if (is.null(name)) name = default_name
   if (is.null(lang)) lang = default_lang
