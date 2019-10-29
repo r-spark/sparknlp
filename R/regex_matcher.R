3d910adf953471b061834755fd20f048c3afc074
Dave Kincaid <kincaid.dave@gmail.com>
2019-10-29 08:51:11 -0500

    RegexMatcher implementation

---
+++ R/regex_matcher.R
@@ -0,0 +1,111 @@
+#' Spark NLP RegexMatcher
+#'
+#' Spark ML estimator that 
+#' See \url{https://nlp.johnsnowlabs.com/docs/en/annotators#regexmatcher}
+#' 
+#' @template roxlate-nlp-algo
+#' @template roxlate-inputs-output-params
+#' @param strategy Can be any of MATCH_FIRST|MATCH_ALL|MATCH_COMPLETE
+#' @param rules_path Path to file containing a set of regex,key pair
+#' @param rules_path_delimiter delimiter between regex and key in the file
+#' @param rules_path_read_as LINE_BY_LINE or SPARK_DATASET
+#' @param rules_path_options options passed to Spark reader if read_as is SPARK_DATASET
+#' 
+#' @export
+nlp_regex_matcher <- function(x, input_cols, output_col,
+                 strategy = NULL, rules_path = NULL, rules_path_delimiter = ",", rules_path_read_as = "LINE_BY_LINE", 
+                 rules_path_options = list("format" = "text"),
+                 uid = random_string("regex_matcher_")) {
+  UseMethod("nlp_regex_matcher")
+}
+
+#' @export
+nlp_regex_matcher.spark_connection <- function(x, input_cols, output_col,
+                 strategy = NULL, rules_path = NULL, rules_path_delimiter = ",", 
+                 rules_path_read_as = "LINE_BY_LINE", rules_path_options = list("format" = "text"),
+                 uid = random_string("regex_matcher_")) {
+
+  args <- list(
+    input_cols = input_cols,
+    output_col = output_col,
+    strategy = strategy,
+    rules_path = rules_path,
+    rules_path_delimiter = rules_path_delimiter,
+    rules_path_read_as = rules_path_read_as,
+    rules_path_options = rules_path_options,
+    uid = uid
+  ) %>%
+  validator_nlp_regex_matcher()
+
+  if (!is.null(args[["rules_path_options"]])) {
+    args[["rules_path_options"]] <- list2env(args[["rules_path_options"]])
+  }
+  
+  jobj <- sparklyr::spark_pipeline_stage(
+    x, "com.johnsnowlabs.nlp.annotators.RegexMatcher",
+    input_cols = args[["input_cols"]],
+    output_col = args[["output_col"]],
+    uid = args[["uid"]]
+  ) %>%
+    sparklyr::jobj_set_param("setStrategy", args[["strategy"]])
+  
+  if (!is.null(args[["rules_path"]])) {
+    sparklyr::invoke(jobj, "setRules", args[["rules_path"]], args[["rules_path_delimiter"]],
+                     read_as(x, args[["rules_path_read_as"]]), args[["rules_path_options"]])
+  }
+
+  new_nlp_regex_matcher(jobj)
+}
+
+#' @export
+nlp_regex_matcher.ml_pipeline <- function(x, input_cols, output_col,
+                 strategy = NULL, rules_path = NULL, rules_path_delimiter = ",", rules_path_read_as = "LINE_BY_LINE", rules_path_options = list("format"="text"),
+                 uid = random_string("regex_matcher_")) {
+
+  stage <- nlp_regex_matcher.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    strategy = strategy,
+    rules_path = rules_path,
+    rules_path_delimiter = rules_path_delimiter,
+    rules_path_read_as = rules_path_read_as,
+    rules_path_options = rules_path_options,
+    uid = uid
+  )
+
+  sparklyr::ml_add_stage(x, stage)
+}
+
+#' @export
+nlp_regex_matcher.tbl_spark <- function(x, input_cols, output_col,
+                 strategy = NULL, rules_path = NULL, rules_path_delimiter = ",", rules_path_read_as = "LINE_BY_LINE", rules_path_options = list("format"="text"),
+                 uid = random_string("regex_matcher_")) {
+  stage <- nlp_regex_matcher.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    strategy = strategy,
+    rules_path = rules_path,
+    rules_path_delimiter = rules_path_delimiter,
+    rules_path_read_as = rules_path_read_as,
+    rules_path_options = rules_path_options,
+    uid = uid
+  )
+
+  stage %>% sparklyr::ml_fit_and_transform(x)
+}
+#' @import forge
+validator_nlp_regex_matcher <- function(args) {
+  args[["input_cols"]] <- cast_string_list(args[["input_cols"]])
+  args[["output_col"]] <- cast_string(args[["output_col"]])
+  args[["strategy"]] <- cast_nullable_string(args[["strategy"]])
+  args[["rules_path"]] <- cast_nullable_string(args[["rules_path"]])
+  args[["rules_path_delimiter"]] <- cast_nullable_string(args[["rules_path_delimiter"]])
+  args[["rules_path_read_as"]] <- cast_choice(args[["rules_path_read_as"]], choices = c("LINE_BY_LINE", "SPARK_DATASET"))
+  args
+}
+
+new_nlp_regex_matcher <- function(jobj) {
+  sparklyr::new_ml_estimator(jobj, class = "nlp_regex_matcher")
+}
---
+++ tests/testthat/testthat-regex-matcher.R
@@ -0,0 +1,54 @@
+setup({
+  sc <- testthat_spark_connection()
+  text_tbl <- testthat_tbl("test_text")
+
+  # These lines should set a pipeline that will ultimately create the columns needed for testing the annotator
+  assembler <- nlp_document_assembler(sc, input_col = "text", output_col = "document")
+
+  pipeline <- ml_pipeline(assembler)
+  test_data <- ml_fit_and_transform(pipeline, text_tbl)
+
+  assign("sc", sc, envir = parent.frame())
+  assign("pipeline", pipeline, envir = parent.frame())
+  assign("test_data", test_data, envir = parent.frame())
+})
+
+teardown({
+  rm(sc, envir = .GlobalEnv)
+  rm(pipeline, envir = .GlobalEnv)
+  rm(test_data, envir = .GlobalEnv)
+})
+
+# test_that("regex_matcher param setting", {
+# # TODO: edit these to make them legal values for the parameters
+#   test_args <- list(
+#     input_cols = c("string1", "string2"),
+#     output_col = "string1",
+#     strategy = "string1",
+#     rules_path = "string1",
+#     rules_path_delimiter = "string1",
+#     rules_path_read_as = c("string1", "string2"),
+#     rules_path_options = c("string1", "string2")
+#   )
+# 
+#   test_param_setting(sc, nlp_regex_matcher, test_args)
+# })
+
+test_that("nlp_regex_matcher spark_connection", {
+  test_annotator <- nlp_regex_matcher(sc, input_cols = c("document"), output_col = "regex", rules_path = here::here("tests", "testthat", "data", "regex_match.txt"))
+  fit_model <- ml_fit(test_annotator, test_data)
+  transformed_data <- ml_transform(fit_model, test_data)
+  expect_true("regex" %in% colnames(transformed_data))
+})
+
+test_that("nlp_regex_matcher ml_pipeline", {
+  test_annotator <- nlp_regex_matcher(pipeline, input_cols = c("document"), output_col = "regex", rules_path = here::here("tests", "testthat", "data", "regex_match.txt"))
+  transformed_data <- ml_fit_and_transform(test_annotator, test_data)
+  expect_true("regex" %in% colnames(transformed_data))
+})
+
+test_that("nlp_regex_matcher tbl_spark", {
+  transformed_data <- nlp_regex_matcher(test_data, input_cols = c("document"), output_col = "regex", rules_path = here::here("tests", "testthat", "data", "regex_match.txt"))
+  expect_true("regex" %in% colnames(transformed_data))
+})
+
