a8c05cca0d874ab1b63a1884b2ed02f7bfcc0d7b
Dave Kincaid <kincaid.dave@gmail.com>
2019-10-31 07:25:41 -0500

    ChunkEmbeddings implementation

--- R/chunk-embeddings.R
+++ R/chunk-embeddings.R
@@ -1,50 +1,49 @@
-#' Spark NLP SentenceEmbeddings
+#' Spark NLP ChunkEmbeddings
 #'
-#' Spark ML transformer that converts the results from WordEmbeddings or BertEmbeddings into sentence or document 
-#' embeddings by either summing up or averaging all the word embeddings in a sentence or a document
-#' (depending on the input_cols).
-#' See \url{https://nlp.johnsnowlabs.com/docs/en/annotators#sentenceembeddings}
+#' Spark ML transformer that utilizes WordEmbeddings or BertEmbeddings to generate chunk embeddings from either Chunker,
+#' NGramGenerator, or NerConverter outputs.
+#' See \url{https://nlp.johnsnowlabs.com/docs/en/annotators#chunkembeddings}
 #' 
 #' @template roxlate-nlp-algo
 #' @template roxlate-inputs-output-params
 #' @param pooling_strategy Choose how you would like to aggregate Word Embeddings to Sentence Embeddings: AVERAGE or SUM
 #' 
 #' @export
-nlp_sentence_embeddings <- function(x, input_cols, output_col,
+nlp_chunk_embeddings <- function(x, input_cols, output_col,
                  pooling_strategy = NULL,
-                 uid = random_string("sentence_embeddings_")) {
-  UseMethod("nlp_sentence_embeddings")
+                 uid = random_string("chunk_embeddings_")) {
+  UseMethod("nlp_chunk_embeddings")
 }
 
 #' @export
-nlp_sentence_embeddings.spark_connection <- function(x, input_cols, output_col,
+nlp_chunk_embeddings.spark_connection <- function(x, input_cols, output_col,
                  pooling_strategy = NULL,
-                 uid = random_string("sentence_embeddings_")) {
+                 uid = random_string("chunk_embeddings_")) {
   args <- list(
     input_cols = input_cols,
     output_col = output_col,
     pooling_strategy = pooling_strategy,
     uid = uid
   ) %>%
-  validator_nlp_sentence_embeddings()
+  validator_nlp_chunk_embeddings()
 
   jobj <- sparklyr::spark_pipeline_stage(
-    x, "com.johnsnowlabs.nlp.embeddings.SentenceEmbeddings",
+    x, "com.johnsnowlabs.nlp.embeddings.ChunkEmbeddings",
     input_cols = args[["input_cols"]],
     output_col = args[["output_col"]],
     uid = args[["uid"]]
   ) %>%
     sparklyr::jobj_set_param("setPoolingStrategy", args[["pooling_strategy"]]) 
 
-  new_nlp_sentence_embeddings(jobj)
+  new_nlp_chunk_embeddings(jobj)
 }
 
 #' @export
-nlp_sentence_embeddings.ml_pipeline <- function(x, input_cols, output_col,
+nlp_chunk_embeddings.ml_pipeline <- function(x, input_cols, output_col,
                  pooling_strategy = NULL,
-                 uid = random_string("sentence_embeddings_")) {
+                 uid = random_string("chunk_embeddings_")) {
 
-  stage <- nlp_sentence_embeddings.spark_connection(
+  stage <- nlp_chunk_embeddings.spark_connection(
     x = sparklyr::spark_connection(x),
     input_cols = input_cols,
     output_col = output_col,
@@ -56,10 +55,10 @@ nlp_sentence_embeddings.ml_pipeline <- function(x, input_cols, output_col,
 }
 
 #' @export
-nlp_sentence_embeddings.tbl_spark <- function(x, input_cols, output_col,
+nlp_chunk_embeddings.tbl_spark <- function(x, input_cols, output_col,
                  pooling_strategy = NULL,
-                 uid = random_string("sentence_embeddings_")) {
-  stage <- nlp_sentence_embeddings.spark_connection(
+                 uid = random_string("chunk_embeddings_")) {
+  stage <- nlp_chunk_embeddings.spark_connection(
     x = sparklyr::spark_connection(x),
     input_cols = input_cols,
     output_col = output_col,
@@ -70,13 +69,13 @@ nlp_sentence_embeddings.tbl_spark <- function(x, input_cols, output_col,
   stage %>% sparklyr::ml_transform(x)
 }
 #' @import forge
-validator_nlp_sentence_embeddings <- function(args) {
+validator_nlp_chunk_embeddings <- function(args) {
   args[["input_cols"]] <- cast_string_list(args[["input_cols"]])
   args[["output_col"]] <- cast_string(args[["output_col"]])
   args[["pooling_strategy"]] <- cast_choice(args[["pooling_strategy"]], choices = c("AVERAGE", "SUM"), allow_null = TRUE)
   args
 }
 
-new_nlp_sentence_embeddings <- function(jobj) {
-  sparklyr::new_ml_transformer(jobj, class = "nlp_sentence_embeddings")
+new_nlp_chunk_embeddings <- function(jobj) {
+  sparklyr::new_ml_transformer(jobj, class = "nlp_chunk_embeddings")
 }
---
+++ tests/testthat/testthat-chunk-embeddings.R
@@ -0,0 +1,53 @@
+setup({
+  sc <- testthat_spark_connection()
+  text_tbl <- testthat_tbl("test_text")
+
+  # These lines should set a pipeline that will ultimately create the columns needed for testing the annotator
+  assembler <- nlp_document_assembler(sc, input_col = "text", output_col = "document")
+  sentdetect <- nlp_sentence_detector(sc, input_cols = c("document"), output_col = "sentence")
+  tokenizer <- nlp_tokenizer(sc, input_cols = c("sentence"), output_col = "token")
+  pos <- nlp_perceptron_pretrained(sc, input_cols = c("sentence", "token"), output_col = "pos")
+  chunker <- nlp_chunker(sc, input_cols = c("sentence", "pos"), output_col = "chunk")
+  embeddings <- nlp_word_embeddings_pretrained(sc, input_cols = c("sentence", "token"), output_col = "embeddings")
+
+  pipeline <- ml_pipeline(assembler, sentdetect, tokenizer, pos, chunker, embeddings)
+  test_data <- ml_fit_and_transform(pipeline, text_tbl)
+
+  assign("sc", sc, envir = parent.frame())
+  assign("pipeline", pipeline, envir = parent.frame())
+  assign("test_data", test_data, envir = parent.frame())
+})
+
+teardown({
+  rm(sc, envir = .GlobalEnv)
+  rm(pipeline, envir = .GlobalEnv)
+  rm(test_data, envir = .GlobalEnv)
+})
+
+test_that("chunk_embeddings param setting", {
+  test_args <- list(
+    input_cols = c("string1", "string2"),
+    output_col = "string1",
+    pooling_strategy = "AVERAGE"
+  )
+
+  test_param_setting(sc, nlp_chunk_embeddings, test_args)
+})
+
+test_that("nlp_chunk_embeddings spark_connection", {
+  test_annotator <- nlp_chunk_embeddings(sc, input_cols = c("chunk", "embeddings"), output_col = "chunk_embeddings")
+  transformed_data <- ml_transform(test_annotator, test_data)
+  expect_true("chunk_embeddings" %in% colnames(transformed_data))
+})
+
+test_that("nlp_chunk_embeddings ml_pipeline", {
+  test_annotator <- nlp_chunk_embeddings(pipeline, input_cols = c("chunk", "embeddings"), output_col = "chunk_embeddings")
+  transformed_data <- ml_fit_and_transform(test_annotator, test_data)
+  expect_true("chunk_embeddings" %in% colnames(transformed_data))
+})
+
+test_that("nlp_chunk_embeddings tbl_spark", {
+  transformed_data <- nlp_chunk_embeddings(test_data, input_cols = c("chunk", "embeddings"), output_col = "chunk_embeddings")
+  expect_true("chunk_embeddings" %in% colnames(transformed_data))
+})
+
