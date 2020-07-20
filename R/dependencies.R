48a73b9406f0196651e36081d72530968d65c857
Dave Kincaid <kincaid.dave@gmail.com>
2020-07-20 13:54:17 -0500

    Upgrade dependency on SparkNLP to 2.5.4

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,4 +1,4 @@
-spark_nlp_version <- "2.5.3"
+spark_nlp_version <- "2.5.4"
 
 spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
--- configure.R
+++ configure.R
@@ -3,21 +3,27 @@
 library(purrr)
 library(dplyr)
 
-spec <- sparklyr::spark_default_compilation_spec() %>%
-  map(function(x) {
-    x$jar_dep <- list.files("~/.m2/repository/com/johnsnowlabs/nlp/spark-nlp_2.11/2.5.2/", full.names = TRUE) %>% 
-      map_chr(normalizePath)
-    x
-  }) %>%
-  keep(~ .x$spark_version >= "2.4.0")
+pkg <- sparklyr:::infer_active_package_name()
+
+spec <- sparklyr::spark_compilation_spec(jar_name = sprintf("%s-2.4-2.11.jar", pkg), 
+                                         jar_path = sparklyr:::find_jar())
+
+#  map(function(x) {
+spec$jar_dep <- list.files("~/.m2/repository/com/johnsnowlabs/nlp/spark-nlp_2.11/2.5.4/", full.names = TRUE) %>% 
+    map_chr(normalizePath)
+#    x
+#  })
+#  keep(~ .x$spark_version >= "2.4.3")
 
 # For some reason this is needed for ContextSpellChecker
-spec <- spec %>% 
-  map(function(x) {
-    x$jar_dep <- append(x$jar_dep, list.files("~/.m2/repository/com/github/universal-automata/liblevenshtein/3.0.0/", full.names = TRUE) %>% 
+#spec <- spec %>% 
+#  map(function(x) {
+spec$jar_dep <- append(spec$jar_dep, list.files("~/.m2/repository/com/github/universal-automata/liblevenshtein/3.0.0/", full.names = TRUE) %>% 
       map_chr(normalizePath))
-    x
-  }) %>% 
-  keep(~ .x$spark_version >= "2.4.0")
+#    x
+#  }) %>% 
+#  keep(~ .x$spark_version >= "2.4.0")
 
 sparklyr::compile_package_jars(spec = spec)
+
+
