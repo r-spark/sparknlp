8a79aa4d670a087e624004f7c163e1323b970a13
Dave Kincaid <kincaid.dave@gmail.com>
2020-10-14 15:42:08 -0500

    Implement BertSentenceEmbeddings annotator

---
+++ tests/testthat/testthat-bert_sentence_embeddings.R
@@ -0,0 +1,29 @@
+setup({
+  sc <- testthat_spark_connection()
+  text_tbl <- testthat_tbl("test_text")
+
+  # These lines should set a pipeline that will ultimately create the columns needed for testing the annotator
+  assembler <- nlp_document_assembler(sc, input_col = "text", output_col = "document")
+
+  pipeline <- ml_pipeline(assembler)
+  test_data <- ml_fit_and_transform(pipeline, text_tbl)
+
+  assign("sc", sc, envir = parent.frame())
+  assign("pipeline", pipeline, envir = parent.frame())
+  assign("test_data", test_data, envir = parent.frame())
+})
+
+teardown({
+  rm(sc, envir = .GlobalEnv)
+  rm(pipeline, envir = .GlobalEnv)
+  rm(test_data, envir = .GlobalEnv)
+})
+
+test_that("nlp_bert_sentence_embeddings pretrained", {
+  model <- nlp_bert_sentence_embeddings_pretrained(sc, input_cols = c("document"), output_col = "bert_sentence_embeddings")
+  transformed_data <- ml_transform(model, test_data)
+  expect_true("bert_sentence_embeddings" %in% colnames(transformed_data))
+  
+  expect_true(inherits(model, "nlp_bert_sentence_embeddings"))
+})
+
