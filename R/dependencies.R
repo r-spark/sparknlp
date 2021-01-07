443837f09258c4ed5ed2a7eee7833d6f6de66ce8
Dave Kincaid <dave@kincaidlabs.ai>
2021-01-07 17:21:44 -0600

    Configs for healthcare licensing

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,17 +1,38 @@
 spark_nlp_version <- "2.6.5"
 
 spark_dependencies <- function(spark_version, scala_version, ...) {
-  sparklyr::spark_dependency(
-    jars = c(
-     system.file(
-       sprintf("java/sparknlp-%s-%s.jar", spark_version, scala_version),
-       package = "sparknlp"
-     )
-    ),
-    packages = c(
-      sprintf("com.johnsnowlabs.nlp:spark-nlp_2.11:%s", spark_nlp_version)
-    )
-  )
+  secretCode <- Sys.getenv("SPARK_NLP_SECRET_CODE", unset = NA)
+  
+  if (is.na(secretCode)) {
+    sparklyr::spark_dependency(
+      jars = c(
+        system.file(
+          sprintf("java/sparknlp-%s-%s.jar", spark_version, scala_version),
+          package = "sparknlp"
+        )
+      ),
+      packages = c(
+        sprintf("com.johnsnowlabs.nlp:spark-nlp_2.11:%s", spark_nlp_version)
+      )
+    )    
+  } else {
+    jsl_version <- strsplit(secretCode, "-")[[1]][1]
+    jsl_url <- paste0("https://pypi.johnsnowlabs.com/", secretCode)
+    
+    sparklyr::spark_dependency(
+      jars = c(
+        system.file(
+          sprintf("java/sparknlp-%s-%s.jar", spark_version, scala_version),
+          package = "sparknlp"
+        )
+      ),
+      packages = c(
+        sprintf("com.johnsnowlabs.nlp:spark-nlp_2.11:%s", spark_nlp_version),
+                "com.johnsnowlabs.nlp:spark-nlp-jsl:2.7.2"
+      ),
+      repositories = c(jsl_url)
+    )  
+  }
 }
 
 #' @import sparklyr
--- README.md
+++ README.md
@@ -23,4 +23,13 @@ The `examples` directory structure here follows the notebook examples found at
 Note that not all the Jupyter notebooks found there have been ported yet, but all functionality still exists in the 
 package.
 
+# Licensed models and annotators
+In order to use the licensed models and annotators you must have a valid license
+from John Snow Labs and have followed the steps in [Spark NLP for Healthcare Getting Started](https://nlp.johnsnowlabs.com/docs/en/licensed_install#install-spark-nlp-for-healthcare).
 
+First you must have setup your AWS-CLI credentials in order to use the 
+licensed pretrained models.
+
+In order to use the licensed annotators you must also setup the environment variable
+SPARK_NLP_SECRET_CODE with the secret code provided by John Snow Labs with your
+license and SPARK_NLP_LICENSE with the license key.
