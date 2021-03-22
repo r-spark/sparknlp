0339586714971b78c7cc4df6d61a2e6c5d9ab7bc
Dave Kincaid <dave@kincaidlabs.ai>
2021-03-22 14:05:42 -0500

    Fixing detection of Spark version

--- R/dependencies.R
+++ R/dependencies.R
@@ -2,15 +2,13 @@ spark_nlp_version <- "3.0.0"
 
 spark_dependencies <- function(spark_version, scala_version, ...) {
   secretCode <- Sys.getenv("SPARK_NLP_SECRET_CODE", unset = NA)
-  
-  # Determine the Spark version so that we insert the correct jar from Maven
-  spark_version_parts <- strsplit(spark_version, "\\.")[[1]]
 
-  if (spark_version_parts[1] == "2" & spark_version_parts[2] == "3") {
+  # Determine the Spark version so that we insert the correct jar from Maven
+  if (spark_version == 2.3) {
     artifact_id <- sprintf("spark-nlp-spark23_%s", scala_version)
-  } else if (spark_version_parts[1] == "2" & spark_version_parts[2] == "4") {
+  } else if (spark_version == 2.4) {
     artifact_id <- sprintf("spark-nlp-spark24_%s", scala_version)    
-  } else if (spark_version_parts[1] == "3") {
+  } else if (spark_version >= 3) {
     artifact_id <- sprintf("spark-nlp_%s", scala_version)
   } else {
     stop(sprintf("Incompatible versions of Spark (%s), Scala (%s) and Spark NLP (%s)!", 
@@ -27,7 +25,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
         )
       ),
       packages = c(
-        sprintf("com.johnsnowlabs.nlp:spark-nlp_%s:%s", scala_version, spark_nlp_version)
+        sprintf("com.johnsnowlabs.nlp:%s:%s", artifact_id, spark_nlp_version)
       )
     )    
   } else {
@@ -43,7 +41,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
         jsl_url
       ),
       packages = c(
-        sprintf("com.johnsnowlabs.nlp:spark-nlp_%s:%s", scala_version, spark_nlp_version)
+        sprintf("com.johnsnowlabs.nlp:%s:%s", artifact_id, spark_nlp_version)
       )
     )  
   }
