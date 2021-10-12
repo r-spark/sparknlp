46844809553a004ad62712925af936df06751565
Dave Kincaid <dave@kincaidlabs.ai>
2021-10-12 08:25:36 -0500

    Adding DistilBertEmbeddings transformer

---
+++ R/distilbert-embeddings.R
@@ -0,0 +1,113 @@
+#' Spark NLP DistilBertEmbeddings
+#'
+#' DistilBERT is a small, fast, cheap and light Transformer model trained by distilling BERT base. 
+#' It has 40% less parameters than bert-base-uncased, runs 60% faster while preserving over 95% of 
+#' BERT's performances as measured on the GLUE language understanding benchmark.
+#' See \url{https://nlp.johnsnowlabs.com/docs/en/transformers#distilbertembeddings}
+#' 
+#' @template roxlate-nlp-algo
+#' @template roxlate-inputs-output-params
+#' @param batch_size Size of every batch (Default depends on model).
+#' @param case_sensitive Whether to ignore case in index lookups (Default depends on model)
+#' @param dimension Number of embedding dimensions (Default depends on model)
+#' @param max_sentence_length Max sentence length to process (Default: 128)
+#' @param storage_ref Unique identifier for storage (Default: this.uid)
+#' 
+#' @export
+nlp_distilbert_embeddings <- function(x, input_cols, output_col,
+                 batch_size = NULL, case_sensitive = NULL, dimension = NULL,  
+                 max_sentence_length = NULL, storage_ref = NULL, 
+                 uid = random_string("distilbert_embeddings_")) {
+  UseMethod("nlp_distilbert_embeddings")
+}
+
+#' @export
+nlp_distilbert_embeddings.spark_connection <- function(x, input_cols, output_col,
+                 batch_size = NULL, case_sensitive = NULL, dimension = NULL, 
+                 max_sentence_length = NULL, output_annotator_type = NULL, storage_ref = NULL, 
+                 uid = random_string("distilbert_embeddings_")) {
+  args <- list(
+    input_cols = input_cols,
+    output_col = output_col,
+    batch_size = batch_size,
+    case_sensitive = case_sensitive,
+    dimension = dimension,
+    max_sentence_length = max_sentence_length,
+    storage_ref = storage_ref,
+    uid = uid
+  ) %>%
+  validator_nlp_distilbert_embeddings()
+
+  jobj <- sparklyr::spark_pipeline_stage(
+    x, "com.johnsnowlabs.nlp.embeddings.DistilBertEmbeddings",
+    input_cols = args[["input_cols"]],
+    output_col = args[["output_col"]],
+    uid = args[["uid"]]
+  ) %>%
+    sparklyr::jobj_set_param("setBatchSize", args[["batch_size"]])  %>%
+    sparklyr::jobj_set_param("setCaseSensitive", args[["case_sensitive"]])  %>%
+    sparklyr::jobj_set_param("setDimension", args[["dimension"]])  %>%
+    sparklyr::jobj_set_param("setMaxSentenceLength", args[["max_sentence_length"]])  %>%
+    sparklyr::jobj_set_param("setStorageRef", args[["storage_ref"]])
+
+  new_nlp_distilbert_embeddings(jobj)
+}
+
+#' @export
+nlp_distilbert_embeddings.ml_pipeline <- function(x, input_cols, output_col,
+                 batch_size = NULL, case_sensitive = NULL, dimension = NULL, 
+                 max_sentence_length = NULL, storage_ref = NULL,
+                 uid = random_string("distilbert_embeddings_")) {
+
+  stage <- nlp_distilbert_embeddings.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    batch_size = batch_size,
+    case_sensitive = case_sensitive,
+    dimension = dimension,
+    max_sentence_length = max_sentence_length,
+    storage_ref = storage_ref,
+    uid = uid
+  )
+
+  sparklyr::ml_add_stage(x, stage)
+}
+
+#' @export
+nlp_distilbert_embeddings.tbl_spark <- function(x, input_cols, output_col,
+                 batch_size = NULL, case_sensitive = NULL, dimension = NULL, 
+                 max_sentence_length = NULL, storage_ref = NULL, 
+                 uid = random_string("distilbert_embeddings_")) {
+  stage <- nlp_distilbert_embeddings.spark_connection(
+    x = sparklyr::spark_connection(x),
+    input_cols = input_cols,
+    output_col = output_col,
+    batch_size = batch_size,
+    case_sensitive = case_sensitive,
+    dimension = dimension,
+    max_sentence_length = max_sentence_length,
+    storage_ref = storage_ref,
+    uid = uid
+  )
+
+  stage %>% sparklyr::ml_transform(x)
+}
+#' @import forge
+validator_nlp_distilbert_embeddings <- function(args) {
+  args[["input_cols"]] <- cast_string_list(args[["input_cols"]])
+  args[["output_col"]] <- cast_string(args[["output_col"]])
+  args[["batch_size"]] <- cast_nullable_integer(args[["batch_size"]])
+  args[["case_sensitive"]] <- cast_nullable_logical(args[["case_sensitive"]])
+  args[["dimension"]] <- cast_nullable_integer(args[["dimension"]])
+  args[["max_sentence_length"]] <- cast_nullable_integer(args[["max_sentence_length"]])
+  args[["storage_ref"]] <- cast_nullable_string(args[["storage_ref"]])
+  args
+}
+
+nlp_float_params.nlp_distilbert_embeddings <- function(x) {
+  return(c())
+}
+new_nlp_distilbert_embeddings <- function(jobj) {
+  sparklyr::new_ml_transformer(jobj, class = "nlp_distilbert_embeddings")
+}
---
+++ tests/testthat/testthat-distilbert-embeddings.R
@@ -0,0 +1,58 @@
+setup({
+  sc <- testthat_spark_connection()
+  text_tbl <- testthat_tbl("test_text")
+
+  # These lines should set a pipeline that will ultimately create the columns needed for testing the annotator
+  assembler <- nlp_document_assembler(sc, input_col = "text", output_col = "document")
+  sentdetect <- nlp_sentence_detector(sc, input_cols = c("document"), output_col = "sentence")
+  tokenizer <- nlp_tokenizer(sc, input_cols = c("sentence"), output_col = "token")
+
+  pipeline <- ml_pipeline(assembler, sentdetect, tokenizer)
+  test_data <- ml_fit_and_transform(pipeline, text_tbl)
+
+  assign("sc", sc, envir = parent.frame())
+  assign("pipeline", pipeline, envir = parent.frame())
+  assign("test_data", test_data, envir = parent.frame())
+})
+
+teardown({
+  rm(sc, envir = .GlobalEnv)
+  rm(pipeline, envir = .GlobalEnv)
+  rm(test_data, envir = .GlobalEnv)
+})
+
+test_that("distilbert_embeddings param setting", {
+
+  test_args <- list(
+    input_cols = c("string1", "string2"),
+    output_col = "string1",
+    batch_size = 100,
+    case_sensitive = FALSE,
+    dimension = 768,
+    #input_annotator_types = c("string1", "string2"),
+    max_sentence_length = 200,
+    #output_annotator_type = "string1",
+    storage_ref = "string1"
+  )
+
+  test_param_setting(sc, nlp_distilbert_embeddings, test_args)
+})
+
+test_that("nlp_distilbert_embeddings spark_connection", {
+  test_annotator <- nlp_distilbert_embeddings(sc, input_cols = c("document", "token"), output_col = "embeddings")
+  transformed_data <- ml_transform(test_annotator, test_data)
+  expect_true("embeddings" %in% colnames(transformed_data))
+  expect_true(inherits(test_annotator, "nlp_distilbert_embeddings"))
+})
+
+test_that("nlp_distilbert_embeddings ml_pipeline", {
+  test_annotator <- nlp_distilbert_embeddings(pipeline, input_cols = c("document", "token"), output_col = "embeddings")
+  transformed_data <- ml_fit_and_transform(test_annotator, test_data)
+  expect_true("embeddings" %in% colnames(transformed_data))
+})
+
+test_that("nlp_distilbert_embeddings tbl_spark", {
+  transformed_data <- nlp_distilbert_embeddings(test_data, input_cols = c("document", "token"), output_col = "embeddings")
+  expect_true("embeddings" %in% colnames(transformed_data))
+})
+
