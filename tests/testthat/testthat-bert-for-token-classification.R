219f05eb201175e8706a0c194a41c0696c0817d9
Dave Kincaid <dave@kincaidlabs.ai>
2021-10-12 10:46:04 -0500

    Add BertForTokenClassification transformer

---
+++ tests/testthat/testthat-bert-for-token-classification.R
@@ -0,0 +1,53 @@
+setup({
+  sc <- testthat_spark_connection()
+  text_tbl <- testthat_tbl("test_text")
+
+  # These lines should set a pipeline that will ultimately create the columns needed for testing the annotator
+  assembler <- nlp_document_assembler(sc, input_col = "text", output_col = "document")
+  sentdetect <- nlp_sentence_detector(sc, input_cols = c("document"), output_col = "sentence")
+  tokenizer <- nlp_tokenizer(sc, input_cols = c("sentence"), output_col = "token")
+
+  pipeline <- ml_pipeline(assembler, sentdetect, tokenizer)
+  test_data <- ml_fit_and_transform(pipeline, text_tbl)
+
+  assign("sc", sc, envir = parent.frame())
+  assign("pipeline", pipeline, envir = parent.frame())
+  assign("test_data", test_data, envir = parent.frame())
+})
+
+teardown({
+  rm(sc, envir = .GlobalEnv)
+  rm(pipeline, envir = .GlobalEnv)
+  rm(test_data, envir = .GlobalEnv)
+})
+
+test_that("bert_for_token_classification param setting", {
+  test_args <- list(
+    input_cols = c("string1", "string2"),
+    output_col = "string1",
+    batch_size = 100,
+    case_sensitive = FALSE,
+    max_sentence_length = 200
+  )
+
+  test_param_setting(sc, nlp_bert_for_token_classification, test_args)
+})
+
+test_that("nlp_bert_for_token_classification spark_connection", {
+  test_annotator <- nlp_bert_for_token_classification(sc, input_cols = c("token", "document"), output_col = "label")
+  transformed_data <- ml_transform(test_annotator, test_data)
+  expect_true("label" %in% colnames(transformed_data))
+  expect_true(inherits(test_annotator, "nlp_bert_for_token_classification"))
+})
+
+test_that("nlp_bert_for_token_classification ml_pipeline", {
+  test_annotator <- nlp_bert_for_token_classification(pipeline, input_cols = c("token", "document"), output_col = "label")
+  transformed_data <- ml_fit_and_transform(test_annotator, test_data)
+  expect_true("label" %in% colnames(transformed_data))
+})
+
+test_that("nlp_bert_for_token_classification tbl_spark", {
+  transformed_data <- nlp_bert_for_token_classification(test_data, input_cols = c("token", "document"), output_col = "label")
+  expect_true("label" %in% colnames(transformed_data))
+})
+
